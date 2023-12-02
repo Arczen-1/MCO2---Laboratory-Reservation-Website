f3ff26079378fda8003eb41c23f1b3008b02402d
Arczen <arczenb@gmail.com>
2023-12-03 03:41:55 +0800

    Revert "Update server.js"
    This reverts commit 7e60e856093f7ff0ab740040989a32e8f26591af.

--- server.js
+++ server.js
@@ -3,22 +3,27 @@ const path = require("path");
 const app = express();
 const port = 3000;
 const hbs = require("hbs");
-const {Users, Seats} = require("./mongodb");
+const {Users, Seats} = require("./Model/mongodb");
 const bodyParser = require("body-parser");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
 const session = require("express-session");
 
 // Set the views directory for your Handlebars templates
-const templatePath = path.join(__dirname, "public", "hbs");
+const templatePath = path.join(__dirname, "View", "public", "hbs");
 app.set("views", templatePath);
 app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "public")));
+
 
 // Set up body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+const staticPath = path.join(__dirname, "View", "public");
+app.use("/static", express.static(staticPath));
+const controllerPath = path.join(__dirname, "Controller");
+app.use("/static/Controller", express.static(controllerPath));
 
 // Validation middleware for the /signup route
 const validateSignup = [
@@ -34,6 +39,7 @@ const validateSignup = [
 
   // Add more validation rules as needed
 ];
+
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname,  "View", "public")));
@@ -79,24 +85,18 @@ app.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-
 app.post("/signup", validateSignup, async (req, res) => {
   const errors = validationResult(req);
-
-  var gotPassword = req.body.password;
-  const hash = bcrypt.hashSync(gotPassword, 10);
-  gotPassword = hash;
-
   if (!errors.isEmpty()) {
     return res.render("signup", { errors: errors.array() });
   }
 
   const data = {
     username: req.body.username,
-    password: gotPassword,
+    password: req.body.password,
     email: req.body.email,
-    course: req.body.course,
-    type: req.body.type
+    course: "",
+    type: ""
   };
 
   try {
@@ -130,18 +130,14 @@ app.get("/login", (req, res) => {
 
 
 app.post("/login", async (req, res) => {
-  const  username = req.body.username;
+  const { username, password } = req.body;
   let isLoggedIn = false; 
   const errors = [];
-
-  var value = req.body.password;
-
-  console.log(value)
 
   try {
     const user = await Users.findOne({ username });
 
-    if (!user || !bcrypt.compareSync(value, user.password)) {
+    if (!user || password !== user.password) {
       errors.push({ msg: "Invalid username or password" });
       res.render("login", { errors });
       return;
@@ -180,7 +176,6 @@ app.get('/view_user', (req, res) => {
 
     res.render('view_user', { currentUser: username, isLoggedIn });
 });
-
 app.get('/edit_user', (req, res) => {
   res.render("edit_user");
 });
@@ -210,26 +205,6 @@ app.post('/edit_user', async (req, res) => {
       console.error('Error updating data in MongoDB:', error);
       res.status(500).render('error', { message: 'Internal Server Error' });
   }
-});
-
-app.get("/delete_user", (req, res) => {
-  res.render("delete_user");
-});
-
-app.post("/delete_user", async (req, res) => {
-  const currentUser = req.session.user; 
-  if (!currentUser) {
-      return res.redirect('/login');
-  }
-  try {
-    await Users.deleteOne({ _id: currentUser._id });
-      req.session.user = null; 
-      const username = "Guest";
-      res.render("homie", { username }); 
-} catch (error) {
-    console.error('Error updating data in MongoDB:', error);
-    res.status(500).render('error', { message: 'Internal Server Error' });
-}
 });
 
 app.get("/reserve", (req, res) => {
@@ -284,7 +259,6 @@ app.get("/reserve", (req, res) => {
     res.status(500).render("error", { message: "Internal Server Error" });
   }
 });
-
 app.post("/reserve-seat", async (req, res) => {
   const { name, seat, selectedDate, isAnonymous, room } = req.body;
 
@@ -385,22 +359,17 @@ app.get("/remove", async (req, res) => {
 });
 
 app.post('/remove-seat', async (req, res) => {
-  const currentUser = req.session.user;
   const { seat, selectedDate } = req.body;
-  const currSeat = await Seats.findOne({seat, reservationDate: selectedDate});
-  console.log(currentUser);
-  console.log(currSeat);
 
   if (!seat || !selectedDate) {
     return res.status(400).json({ error: 'Seat and selected date information are required.' });
   }
+
   try {
-    if (currentUser.username == currSeat.name || currentUser.type == "Teacher") {
-      await Seats.deleteOne({ seat, reservationDate: selectedDate });
-      res.json({ message: 'Seat reservation removed successfully.' });
-    }else{
-      res.json({ message: 'You have are not allowed to remove this reservation.' });
-    }
+    
+    await Seats.deleteOne({ seat, reservationDate: selectedDate });
+
+    res.json({ message: 'Seat reservation removed successfully.' });
   } catch (error) {
     console.error('Error removing seat reservation:', error);
     res.status(500).json({ error: 'Internal Server Error' });
@@ -474,7 +443,114 @@ app.get("/view_slot", async (req, res) => {
 
 
 
+app.get("/306A", async (req, res) => {
+  try {
+    
+    const seats = [
+      "A1",
+      "A2",
+      "A3",
+      "A4",
+      "A5",
+      "A6",
+      "A7",
+      "A8",
+      "A9",
+      "A10",
+      "B1",
+      "B2",
+      "B3",
+      "B4",
+      "B5",
+      "B6",
+      "B7",
+      "B8",
+      "B9",
+      "B10",
+      "C1",
+      "C2",
+      "C3",
+      "C4",
+      "C5",
+      "C6",
+      "C7",
+      "C8",
+      "C9",
+      "C10",
+      "D1",
+      "D2",
+      "D3",
+      "D4",
+      "D5",
+      "D6",
+      "D7",
+      "D8",
+      "D9",
+      "D10",
+    ];
+    console.log("Seats:", seats);
+    res.render("GK306A", { seats });
+  } catch (error) {
+    console.error("Error rendering reserve page:", error);
+    res.status(500).render("error", { message: "Internal Server Error" });
+  }
+});
+app.get("/302B", async (req, res) => {
+  try {
+    
+    const seats = [
+      "A1",
+      "A2",
+      "A3",
+      "A4",
+      "A5",
+      "A6",
+      "A7",
+      "A8",
+      "A9",
+      "A10",
+      "B1",
+      "B2",
+      "B3",
+      "B4",
+      "B5",
+      "B6",
+      "B7",
+      "B8",
+      "B9",
+      "B10",
+      "C1",
+      "C2",
+      "C3",
+      "C4",
+      "C5",
+      "C6",
+      "C7",
+      "C8",
+      "C9",
+      "C10",
+      "D1",
+      "D2",
+      "D3",
+      "D4",
+      "D5",
+      "D6",
+      "D7",
+      "D8",
+      "D9",
+      "D10",
+    ];
+    console.log("Seats:", seats);
+    res.render("GK302B", { seats });
+  } catch (error) {
+    console.error("Error rendering reserve page:", error);
+    res.status(500).render("error", { message: "Internal Server Error" });
+  }
+});
+
+
+
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log("Server is running at http://localhost:${port}");
 });
