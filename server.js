8cbc0d44bdfe1b4ae1b824c46c92de25f6d994bb
jjacobdela <cobdelapena@gmail.com>
2023-12-04 03:04:34 +0800

    remove reservation

--- server.js
+++ server.js
@@ -696,6 +696,118 @@ app.get("/view_slot_GK302B", async (req, res) => {
   res.render("view_slot_GK302B");
 });
 
+app.get("/remove_reservation_GK302B", async (req, res) => {
+  try {
+    const seats = [
+      "A1",
+      "A2",
+      "A3",
+      "A4",
+      "A5",
+      "A6",
+      "A7",
+      "A8",
+      "A9",
+      "A10",
+      "B1",
+      "B2",
+      "B3",
+      "B4",
+      "B5",
+      "B6",
+      "B7",
+      "B8",
+      "B9",
+      "B10",
+      "C1",
+      "C2",
+      "C3",
+      "C4",
+      "C5",
+      "C6",
+      "C7",
+      "C8",
+      "C9",
+      "C10",
+      "D1",
+      "D2",
+      "D3",
+      "D4",
+      "D5",
+      "D6",
+      "D7",
+      "D8",
+      "D9",
+      "D10",
+    ];
+  console.log("Seats:", seats);
+  res.render("remove_reservation_GK302B", { seats });
+  }catch (error) {
+    console.error("Error rendering reserve page:", error);
+    res.status(500).render("error", { message: "Internal Server Error" });
+  }
+});
+
+app.get("/remove_reservation_GK306A", async (req, res) => {
+  try {
+    const seats = [
+      "A1",
+      "A2",
+      "A3",
+      "A4",
+      "A5",
+      "A6",
+      "A7",
+      "A8",
+      "A9",
+      "A10",
+      "B1",
+      "B2",
+      "B3",
+      "B4",
+      "B5",
+      "B6",
+      "B7",
+      "B8",
+      "B9",
+      "B10",
+      "C1",
+      "C2",
+      "C3",
+      "C4",
+      "C5",
+      "C6",
+      "C7",
+      "C8",
+      "C9",
+      "C10",
+      "D1",
+      "D2",
+      "D3",
+      "D4",
+      "D5",
+      "D6",
+      "D7",
+      "D8",
+      "D9",
+      "D10",
+    ];
+  console.log("Seats:", seats);
+  res.render("remove_reservation_GK306A", { seats });
+  }catch (error) {
+    console.error("Error rendering reserve page:", error);
+    res.status(500).render("error", { message: "Internal Server Error" });
+  }
+});
+
+app.get("/remove_reservation_GK306A", async (req, res) => {
+  res.render("remove_reservation_GK306A");
+});
+
+app.get("/remove_reservation_GK302B", async (req, res) => {
+  res.render("remove_reservation_GK302B");
+});
+
 // Start the server
 app.listen(port, () => {
   console.log("Server is running at http://localhost:${port}");
